837403fbcfc4d9a839ad9b4642d40eb120dc2067
AbstractSolution <134761506+AbstractSolution@users.noreply.github.com>
2025-01-12 13:23:42 -0800

    Update simlab2_ex1.cpp - command injection fix 2

--- simlab2_ex1.cpp
+++ simlab2_ex1.cpp
@@ -3,6 +3,20 @@
 #include <stdlib.h>
  
 #define BUFSIZE 256
+
+
+int sanitizeWhitelist(char *user_data, char *ok_chars)
+{
+    for (char *cp = user_data; *cp != '\0'; cp++)
+    {
+        if (strchr(ok_chars, *cp) == NULL)
+        {
+            fprintf(stderr, "Please provide the address of a file as an input.\n");
+            return -1;
+        }
+    }
+    return 0;
+}
     
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
@@ -14,7 +28,7 @@ int main(int argc, char** argv) {
 
     static char ok_chars[] = "abcdefghijklmnopqrstuvwxyz"
                              "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                             "1234567890_-.@";
+                             "1234567890_-.@"; // Whitelist characters to sanitize the input.
     char *user_data = argv[1];
     char *cp = user_data; /* Cursor into string */
     for (char *cp = user_data; *cp != '\0'; cp++) {
@@ -30,9 +44,16 @@ int main(int argc, char** argv) {
     }
     else
     {
-        char cmd[BUFSIZE] = "wc -c < ";
-        strncat(cmd, argv[1], 247);
-        system(cmd);
+        if (sanitizeWhitelist(argv[1], ok_chars) != 0)
+        {
+           return -1;
+        }
+        else
+        {
+           char cmd[BUFSIZE] = "wc -c < ";
+           strncat(cmd, argv[1], 247);
+           system(cmd);
+        }
     }
  
     
